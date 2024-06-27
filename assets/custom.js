f55f805cb0842b85048c0a8eab225c71339e44a0
Pratik <pratik.shopifydev@gmail.com>
2024-06-27 18:19:52 +0530

    Three section creat on PDP & Footer design

--- assets/custom.js
+++ assets/custom.js
@@ -1,3 +1,5 @@
+// TOGGLE SWITCH JS START
+
 // Add event listener to toggle dark mode
 document.getElementById('modeToggle').addEventListener('change', function() {
     const body = document.body;
@@ -19,5 +21,25 @@ function applyDarkMode() {
         body.classList.remove('dark-mode');
         modeToggle.checked = false;
     }
-
 }
+
+// TOGGLE SWITCH JS END
+
+// EXTRA STRAP MODAL OPEN/CLOSE JS START
+
+document.querySelector(".product-block--strap_heading").addEventListener("click", function() {
+    document.querySelector(".strap_modal").style.display = "block";
+    document.querySelector(".strap_modal_overlay").classList.add("active");
+});
+
+document.querySelector(".strap_modal .close").addEventListener("click", function() {
+    document.querySelector(".strap_modal").style.display = "none";
+    document.querySelector(".strap_modal_overlay").classList.remove("active");
+});
+
+document.querySelector(".strap_modal_overlay").addEventListener("click", function() {
+    document.querySelector(".strap_modal").style.display = "none";
+    this.classList.remove("active");
+});
+
+// EXTRA STRAP MODAL OPEN/CLOSE JS END
